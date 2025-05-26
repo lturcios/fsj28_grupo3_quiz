1bc586a44d8277a1ba7e849f61b694573a55f382
ManuHernandeeez <manu.hernandez.15.2002@gmail.com>
2025-05-25 20:48:42 -0600

    Mostrar preguntas por categoria

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,7 @@
 import { quizPreguntas } from "./preguntas";
 
 const body = document.body;
+
 const bienvenida = document.querySelector(".bienvenida");
 const lista_categorias = document.querySelector(".lista-categorias");
 const quiz = document.querySelector(".quiz");
@@ -10,23 +11,50 @@ const numero_pregunta_actual = document.getElementById("numero-pregunta-actual")
 const numero_total_pregunta = document.getElementById("numero-pregunta-total");
 const texto_pregunta = document.getElementById("texto-pregunta");
 const opciones = document.querySelector(".opciones");
-const opcion = document.querySelector(".opcion");
 const boton_enviar = document.querySelector(".boton-enviar");
 const progressbar = document.querySelector(".progressbar");
 
-let quizActual = null;
+// manejar preguntas del quizz
+let preguntasActuales = [];
 let indicePreguntaActual = 0;
 let puntaje = 0;
 let respuestaSeleccionada = null;
+let categoriaSeleccionada = null;
 
-// xxxxxxxx
-let quizIniciado = false;
+// ocultar quizz al terminar
+quiz.style.display = "none";
+resultado.style.display = "none";
 
-
-function cargarCategorias(){
-    lista_categorias.innerHTML = '';
-    quizPreguntas.forEach( category => {
-        const categoria = document.querySelector(".categoria");
-        
-    })
+// Mostrar pregunta actual
+function mostrarPregunta() {
+    const pregunta = preguntasActuales[indicePreguntaActual];
+    texto_pregunta.textContent = pregunta.pregunta;
+    const opcionesDOM = opciones.querySelectorAll('.opcion');
+    opcionesDOM.forEach((opcionDOM, i) => {
+        opcionDOM.querySelector('.texto-respuesta').textContent = pregunta.opciones[i];
+        opcionDOM.classList.remove('seleccionada');
+        opcionDOM.querySelectorAll('.icono-feedback').forEach(icono => icono.style.display = 'none');
+    });
+    numero_pregunta_actual.textContent = indicePreguntaActual + 1;
+    numero_total_pregunta.textContent = preguntasActuales.length;
+    progressbar.style.width = `${((indicePreguntaActual + 1) / preguntasActuales.length) * 100}%`;
+    respuestaSeleccionada = null;
+    document.querySelector('.mensaje-feedback').textContent = '';
 }
+
+// seleccionar cate
+lista_categorias.addEventListener('click', (e) => {
+    const categoriaDiv = e.target.closest('.categoria');
+    if (!categoriaDiv) return;
+    categoriaSeleccionada = categoriaDiv.dataset.categoria;
+
+
+    const categoriaObj = quizPreguntas.find(cat => cat.categoria.toLowerCase() === categoriaSeleccionada);
+    preguntasActuales = categoriaObj.preguntas;
+    indicePreguntaActual = 0;
+    puntaje = 0;
+    bienvenida.classList.remove('active');
+    quiz.style.display = 'block';
+    resultado.style.display = 'none';
+    mostrarPregunta();
+});
